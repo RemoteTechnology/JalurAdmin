0b7b6b7cedc94a8a063e146ca0336b9e2f678039
VuacheslavMironov <vuacheslav.mir@gmail.com>
2024-03-14 23:38:31 +0700

    Поправил ресурс

--- src/app/Http/Resources/User/UserResource.php
+++ src/app/Http/Resources/User/UserResource.php
@@ -14,22 +14,6 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-
-            'id'                => $this->id,
-            'first_name'        => $this->first_name,
-            'last_name'         => $this->last_name,
-            'middle_name'       => $this->middle_name,
-            'age'               => $this->age,
-            'role'              => $this->role,
-            'gender'            => $this->gender,
-            'image'             => $this->image,
-            'weight'            => $this->weight,
-            'height'            => $this->height,
-            'size_cloth'        => $this->size_cloth,
-            'phone'             => $this->phone,
-            'code'              => $this->code,
-            'password_admin'    => $this->password_admin,
-        ];
+        return parent::toArray($request);
     }
 }
